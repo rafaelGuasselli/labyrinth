2d2bcaa83808e8eb56056ff42343e6c4f2dbad5c
rafaelguasselli <rafael.g.l.silva@gmail.com>
2023-10-15 23:48:01 -0300

    a

--- map.py
+++ map.py
@@ -3,17 +3,20 @@ from labyrinth_generator import LabyrinthGenerator
 
 class Map:
 	def __init__(self, size):
-		self.createEmptyMap()
+		self.height, self.width = size
+		self.map = self.createEmptyMap(size)
 		self.generator = LabyrinthGenerator()
 		self.generator.generate(self.map, size)
 
-	def createEmptyMap(self):
-		self.map = []
-		for i in range(0, self.height):
+	def createEmptyMap(self, size):
+		height, width = size
+		map = []
+		for i in range(0, height):
 			line = []
-			for j in range(0, self.width):
+			for j in range(0, width):
 				line.append(None)
-			self.map.append(line)
+			map.append(line)
+		return map
 
 	def render(self):		
 		string = ""
